d33df4c1003584ee33ead346be96a3919df9badc
Szymon Rączka <04_barista_pads@icloud.com>
2026-03-25 17:04:10 +0100

    Revert "feat: add all extensions and skills from espennilsen/pi"
    This reverts commit 0dd191738dac3b740db282963fe6f25887041adb.

--- extensions/pi-brave-search/src/logger.ts
+++
@@ -1,8 +0,0 @@
-import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-
-const CHANNEL = "brave-search";
-
-export function createLogger(pi: ExtensionAPI) {
-	return (event: string, data: unknown, level = "INFO") =>
-		pi.events.emit("log", { channel: CHANNEL, event, level, data });
-}
